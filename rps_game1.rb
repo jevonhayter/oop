4b89fd2eda6131b42708ccaff01cb87b0838e17d
Jevon Hayter <jevonhayter@gmail.com>
2016-11-16 19:14:16 -0500

    updated apps

--- rps_game1.rb
+++ rps_game1.rb
@@ -1,3 +1,39 @@
+class Move
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_move)
+     (rock? && other_move.scissors?) ||
+       (paper? && other_move.rock?) ||
+       (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?) 
+  end
+
+  def to_s
+    @value
+  end
+end
+
 class Player
   attr_accessor :move, :name
 
@@ -24,10 +60,10 @@ class Human < Player
       loop do 
         puts 'Please choose rock, paper, or scissors:'
         choice = gets.chomp
-        break if ['rock', 'paper', 'scissors'].include?(choice)
+        break if Move::VALUES.include?(choice)
         puts 'Sorry, invalid pick'
       end
-      self.move = choice
+      self.move = Move.new(choice)
   end
 end
 
@@ -37,7 +73,7 @@ class Computer < Player
   end
 
   def choose
-    self.move = ['rock', 'paper', 'scissors'].sample
+    self.move = Move.new(Move::VALUES.sample)
   end
 end
 
@@ -61,19 +97,12 @@ class RPSGame
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}"
 
-    case human.move
-    when 'rock'
-      puts "It's a tie!" if computer.move == 'rock'
-      puts "#{human.name} won!" if computer.move == 'scissors'
-      puts "#{computer.name} won!" if computer.move == 'paper'
-    when 'paper'
-      puts "It's a tie!" if computer.move == 'paper'
-      puts "#{human.name} won!" if computer.move == 'rock'
-      puts "#{computer.name} won!" if computer.move == 'scissors'
-    when 'scissors'
-      puts "It's a tie!" if computer.move == 'scissors'
-      puts "#{human.name} won!" if computer.move == 'paper'
-      puts "#{computer.name} won!" if computer.move == 'rock'
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
     end
   end
 
@@ -86,8 +115,8 @@ class RPSGame
       puts 'Sorry, must be y or n.'
     end
 
-    return true if answer == 'y'
-    return false
+    return false if answer.downcase == 'n'
+    return true if answer.downcase == 'y'
   end
 
   def play
