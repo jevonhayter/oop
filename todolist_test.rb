5e88a141c812f879f444211cfd8fefa0f34a0f07
Jevon Hayter <jevonhayter@gmail.com>
2016-12-01 17:22:17 -0500

    Testing

--- todolist_test.rb
+++ todolist_test.rb
@@ -50,4 +50,93 @@ class TodoListTest < MiniTest::Test
   def test_done?
     assert_equal(false, @list.done?)
   end
+
+  def test_type_error?
+    assert_raises(TypeError) do
+      name = 'Jae'
+    @list.add(name)
+    end
+  end
+
+  def test_shovel
+    @list << (@todo1)
+    assert_equal(4, @list.size)
+  end
+
+  def test_add
+    @list.add(@todo1)
+    assert_equal(4, @list.size)
+  end
+
+  def test_item_at
+    assert_raises(IndexError) { @list.item_at(100) }
+    assert_equal(@todo1, @list.item_at(0))
+    assert_equal(@todo2, @list.item_at(1))
+  end
+
+  def mark_done_at
+    assert_raises(IndexError) { @list.mark_done_at(100) }
+    @list.mark_done_at(1)
+    assert_equal(false, @todo1.done?)
+    assert_equal(true, @todo2.done?)
+    assert_equal(false, @todo3.done?)
+  end
+
+  def test_mark_undone_at
+    assert_raises(IndexError) { @list.mark_undone_at(100) }
+    @todo1.done!
+    @todo2.done!
+    @todo3.done!
+
+    @list.mark_undone_at(1)
+
+    assert_equal(true, @todo1.done?)
+    assert_equal(false, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+  end
+
+  def test_done!
+    @list.each { |i| i.done! }
+
+    assert_equal(true, @todo1.done?)
+    assert_equal(true, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+  end
+
+  def test_remove_at
+    assert_raises(IndexError) { @list.remove_at(100) }
+    assert_equal(@todo1, @list.remove_at(0))
+  end
+
+  def test_to_s_3
+  output = <<-OUTPUT.chomp.gsub /^\s+/, ""
+  ---- Today's Todos ----
+  [X] Buy milk
+  [X] Clean room
+  [X] Go to gym
+  OUTPUT
+
+  @list.done!
+  assert_equal(output, @list.to_s)
+  end
+
+  def test_each
+    counter = 0
+    @list.each { counter += 1 }
+    assert_equal(3, counter)
+  end
+
+  def test_each_2
+    turn = @list.each { |i| nil }
+    assert_equal(@list, turn)
+  end
+
+  def test_select
+    @todo1.done!
+    list = TodoList.new(@list.title)
+    list.add(@todo1)
+
+    assert_equal(list.title, @list.title)
+    assert_equal(list.to_s, @list.select{ |todo| todo.done? }.to_s)
+  end
 end
